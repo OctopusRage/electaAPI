40df801797b35191b71738a2ff80c10bdac38329
octopusRage <ahyana1995@gmail.com>
2016-11-28 02:42:53 -0500

    top page

--- app/controllers/api/v1/analyzes/dashboard_page_controller.rb
+++ app/controllers/api/v1/analyzes/dashboard_page_controller.rb
@@ -4,6 +4,12 @@ class Api::V1::Analyzes::DashboardPage < ApplicationController
 		follower_count = UserFollower.where(following_id: current_user.id).count
     following_count = UserFollower.where(follower_id: current_user.id).count
     total_vote = current_user.votes.all.count
-    today_participant_count = UserVote.where()
+    today_participant_count = Vote.select("user_votes.*").joins(:user_votes).where(user_id: current_user.id).count
+		render json: {
+			follower_count: follower_count,
+			following_count: following_count,
+			total_vote: total_vote,
+			today_participant_count: today_participant_count
+		}    
 	end
 end
