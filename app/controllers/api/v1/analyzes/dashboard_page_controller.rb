76dcdd4fc4b621e950cb426c083b843086f7aff7
octopusRage <ahyana1995@gmail.com>
2016-12-16 10:30:28 -0500

    tmp fix for nil data

--- app/controllers/api/v1/analyzes/dashboard_page_controller.rb
+++ app/controllers/api/v1/analyzes/dashboard_page_controller.rb
@@ -25,49 +25,58 @@ class Api::V1::Analyzes::DashboardPageController < ApplicationController
 		else
 			vote_id = current_user.votes.last.id
 		end
-		vote = Vote.find(vote_id)
-		participant_count = vote.user_votes.count
-		today_participant_count = UserVote.where("DATE(user_votes.created_at) = ? AND vote_id = ?", DateTime.now.to_date, vote_id).count
-		user_vote =  UserVote.joins(:user).where("vote_id = ?", vote_id)
-		top_education = user_vote.order("count_all DESC").group(:degree).count.try(:first).try(:first)
-		top_profesion = user_vote.order("count_all DESC").group(:job).count.try(:first).try(:first)
-		y_filter = params[:y_filter]
-		x_filter = params[:x_filter]
-		grouped_query = ""
-
-		case x_filter
-		when "years"
-			grouped_query = "YEARS(user_votes.created_at)"
-		when "month"
-			grouped_query = "MONTH(user_votes.created_at)"
-		else
-			grouped_query = "DATE(user_votes.created_at)"
-		end
-
-		filtered = user_vote.group(grouped_query, :gender).count if y_filter=="gender"
-		filtered = user_vote.group(grouped_query, :degree,).count if y_filter=="degree"
-		filtered = user_vote.group(grouped_query, :job).count if y_filter=="job"
-			hash_result = {}
-		filtered.map { |e| 
-			tmp_key_prim = e.first[0]
-			tmp_key = e.first[1]
-			tmp_val = e.second
-			hash_result = hash_result.merge("#{tmp_key_prim}" => {"#{tmp_key}" => tmp_val})
-		}
-		filtered = UserVote.joins(:vote_option).where("vote_options.vote_id = ?", vote_id).group(grouped_query, :vote_option).count if y_filter== "options"
-		render json:{
-			status: 'success',
-			data: {
-				stat: {
-					participant_count: participant_count,
-					today_participant_count: today_participant_count,
-					top_profesion: top_profesion,
-					top_education: top_education
-				},
-				chart: {
-					filtered: hash_result
+		if vote_id.nil?
+			render json: {
+				status: 'success', 
+				data: {
+					empty_data: true
 				}
+			}, status: 200
+		else
+			vote = Vote.find(vote_id)
+			participant_count = vote.user_votes.count
+			today_participant_count = UserVote.where("DATE(user_votes.created_at) = ? AND vote_id = ?", DateTime.now.to_date, vote_id).count
+			user_vote =  UserVote.joins(:user).where("vote_id = ?", vote_id)
+			top_education = user_vote.order("count_all DESC").group(:degree).count.try(:first).try(:first)
+			top_profesion = user_vote.order("count_all DESC").group(:job).count.try(:first).try(:first)
+			y_filter = params[:y_filter]
+			x_filter = params[:x_filter]
+			grouped_query = ""
+
+			case x_filter
+			when "years"
+				grouped_query = "YEARS(user_votes.created_at)"
+			when "month"
+				grouped_query = "MONTH(user_votes.created_at)"
+			else
+				grouped_query = "DATE(user_votes.created_at)"
+			end
+
+			filtered = user_vote.group(grouped_query, :gender).count if y_filter=="gender"
+			filtered = user_vote.group(grouped_query, :degree,).count if y_filter=="degree"
+			filtered = user_vote.group(grouped_query, :job).count if y_filter=="job"
+				hash_result = {}
+			filtered.map { |e| 
+				tmp_key_prim = e.first[0]
+				tmp_key = e.first[1]
+				tmp_val = e.second
+				hash_result = hash_result.merge("#{tmp_key_prim}" => {"#{tmp_key}" => tmp_val})
 			}
-		}, status: 200
+			filtered = UserVote.joins(:vote_option).where("vote_options.vote_id = ?", vote_id).group(grouped_query, :vote_option).count if y_filter== "options"
+			render json:{
+				status: 'success',
+				data: {
+					stat: {
+						participant_count: participant_count,
+						today_participant_count: today_participant_count,
+						top_profesion: top_profesion,
+						top_education: top_education
+					},
+					chart: {
+						filtered: hash_result
+					}
+				}
+			}, status: 200
+		end
 	end
 end
