6eda10309ef73805929e3fa8693e989d2f363ae9
OctopusPC <octopuspc@localhost.localdomain>
2016-12-17 16:48:07 +0700

    fixing mis id when create message

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+
   VALID_GENDER = %w(male female)
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   DEFAULT_DATE_OF_BIRTH = "1970-01-01T07:00:00Z"
@@ -11,27 +11,27 @@ class User < ActiveRecord::Base
   DEFAULT_ORIGIN_LNG  = 106.8403
   AVAILABLE_DEGREE = [
     "SD","SMP","SMA","S1","S2","S3"
-  ]  
+  ]
   AVAILABLE_JOB = [
     "pns", "karyawan"
-  ]  
+  ]
 
   before_create :set_auth_token, :set_lower_email, :set_lower_job
-  
+
   validates :name, presence: true
   validates :gender, inclusion: { in: VALID_GENDER },
     allow_blank: true, case_sensitive: false
   validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
- 
-  
+
+
 
   belongs_to :user_type
   has_many :file_uploads, as: :uploader
   has_many :user_follower, foreign_key: :follower_id, class_name: "UserFollower"
-  has_many :votes  
-  has_many :user_votes  
-  has_many :messages, foreign_key: "to", class_name: "Message"
+  has_many :votes
+  has_many :user_votes
+  has_many :messages, foreign_key: "from", class_name: "Message"
 
   def set_lower_email
     self.email = self.email.downcase
@@ -42,7 +42,7 @@ class User < ActiveRecord::Base
       self.job = self.job.downcase
     end
   end
-    
+
   def image(base_url=nil)
     base_url = ENV["AVATAR_BASE_URL"]
     url = "#{base_url}#{avatar_url}" if avatar_url
@@ -69,7 +69,7 @@ class User < ActiveRecord::Base
 
   def as_simple_json(options={})
     {
-      id: id, 
+      id: id,
       name: name,
       image: image
     }
@@ -102,7 +102,7 @@ class User < ActiveRecord::Base
       gender: gender,
       age: age(date_of_birth) || 0,
       image: image(options[:base_url]),
-      city: city, 
+      city: city,
       province: province ||""
     }
   end
