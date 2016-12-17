488b0dcda93596daf63edf6ad07c262d00459c88
octopus <octopus@localhost.localdomain>
2016-12-17 13:12:58 -0500

    improving message API

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,12 @@
 class Message < ActiveRecord::Base
-	
+	def as_json(options={})
+		{
+			id: id, 
+			to: to, 
+			to_name: User.find(to).name,
+			from: from,
+			message: message,
+			created_at: created_at
+		}
+	end	
 end
