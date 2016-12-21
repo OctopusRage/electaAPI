ce4d1cb30b4575c778f0ac13edfe8cb8482df539
octopus <octopus@localhost.localdomain>
2016-12-21 13:26:09 -0500

    improving dashboard page response

--- app/controllers/api/v1/analyzes/dashboard_page_controller.rb
+++ app/controllers/api/v1/analyzes/dashboard_page_controller.rb
@@ -37,8 +37,9 @@ class Api::V1::Analyzes::DashboardPageController < ApplicationController
 			vote_title = vote.title
 			participant_count = vote.user_votes.count
 			today_participant_count = UserVote.where("DATE(user_votes.created_at) = ? AND vote_id = ?", DateTime.now.to_date, vote_id).count
-			user_vote =  UserVote.joins(:user).where("vote_id = ?", vote_id)
-			top_education = user_vote.order("count_all DESC").group(:degree).count.try(:first).try(:first)
+			user_vote =  User.joins("LEFT JOIN user_votes ON users.id = user_votes.user_id AND user_votes.vote_id = #{vote_id}")
+			user_vote_cat =  UserVote.joins(:user).where("user_votes.vote_id  = ?", vote_id)
+			top_education = user_vote.order("count_all DESC").group(:degree).count.try(:first).try(:first) || ""
 			top_profesion = user_vote.order("count_all DESC").group(:job).count.try(:first).try(:first) || ""
 
 			modus_choice = vote.vote_options.as_json
@@ -67,18 +68,41 @@ class Api::V1::Analyzes::DashboardPageController < ApplicationController
 			end
 
 			filtered = user_vote.group(grouped_query, grouped_query_y).count
-			max_category = user_vote.group(grouped_query_y).order("count_all DESC").count.try(:first) || ""
+			max_category = user_vote_cat.group(grouped_query_y).order("count_all DESC").count.try(:first) || ""
 			max_category = ["", ""] if max_category.nil?
-			min_category = user_vote.group(grouped_query_y).order("count_all ASC").count.try(:first) || ""
+			min_category = user_vote_cat.group(grouped_query_y).order("count_all ASC").count.try(:first) || ""
 			min_category = ["", ""] if min_category.nil?
-				hash_result = {}
+			hash_result = {}
+			hash_extra = {}
 			filtered.map { |e|
-				tmp_key_prim = e.first[0]
-				tmp_key = e.first[1]
-				tmp_val = e.second
-				hash_result = hash_result.merge("#{tmp_key_prim}" => {"#{tmp_key}" => tmp_val})
+				if e.first[0] # if there's nil record then save to tmp_nil value
+					tmp_key_prim = e.first[0]
+					tmp_key = e.first[1]
+					tmp_val = e.second
+					hash_result = hash_result.merge("#{tmp_key_prim}" => {"#{tmp_key}" => tmp_val})\
+				else
+					hash_extra = hash_extra.merge({"#{e.first[1]}" => 0})
+				end
 			}
-			filtered = UserVote.joins(:vote_option).where("vote_options.vote_id = ?", vote_id).group(grouped_query, :vote_option).count if y_filter== "options"
+			hash_result_final = {}
+			if hash_extra.count > 0
+				hash_result.each { |e| 
+					tmp_hash = {}
+					hash_extra.each { |e1| 
+						if !e.second.key?(e1.first) 
+							tmp_hash = tmp_hash.merge(e.second.merge({"#{e1.first}"=> e1.second}))
+						else
+							tmp_hash = tmp_hash.merge(e.second)
+						end
+					}
+					hash_result_final = hash_result_final.merge({"#{e.first}"=> tmp_hash})
+				}
+			else
+				hash_result_final = hash_result
+			end
+			
+
+			#filtered = UserVote.joins(:vote_option).where("vote_options.vote_id = ?", vote_id).group(grouped_query, :vote_option).count if y_filter== "options"
 			render json:{
 				status: 'success',
 				data: {
@@ -93,9 +117,7 @@ class Api::V1::Analyzes::DashboardPageController < ApplicationController
 						max_value: max_category,
 						min_value: min_category
 					},
-					chart: {
-						filtered: hash_result
-					}
+					chart: hash_result_final
 				}
 			}, status: 200
 		end
