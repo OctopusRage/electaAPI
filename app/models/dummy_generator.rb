32d0a6a7bb5ba7d3135e8e62e5402bf417fa3728
OctopusPC <octopuspc@localhost.localdomain>
2016-12-27 00:53:18 +0700

    add generator

--- app/models/dummy_generator.rb
+++ app/models/dummy_generator.rb
@@ -1,7 +1,9 @@
 class DummyGenerator
-  def self.generateUserVote(from, to)
+  def self.generateUserVote(from, to, target)
+    vote = Vote.find(target)
+    vote_length = vote.vote_options.count
     (from..to).each do |i|
-
+      UserVote.participate(User.find(i), target, generateRandom(0, vote_length-1))
     end
   end
 
