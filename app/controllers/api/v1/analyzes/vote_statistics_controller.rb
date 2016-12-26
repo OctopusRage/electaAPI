9d974310159f0161f3dca81b3142002b5348d477
OctopusPC <octopuspc@localhost.localdomain>
2016-12-26 21:15:27 +0700

    try changing dashboard page logic

--- app/controllers/api/v1/analyzes/vote_statistics_controller.rb
+++ app/controllers/api/v1/analyzes/vote_statistics_controller.rb
@@ -14,12 +14,12 @@ class Api::V1::Analyzes::VoteStatisticsController < ApplicationController
 		else
 			vote_title = vote.title
 			participant_count = vote.user_votes.count
-			
+
 			y_filter = params[:y_filter]
 			x_filter = params[:x_filter]
 			grouped_query = ""
 
-			case x_filter	
+			case x_filter
 			when "years"
 				grouped_query = "EXTRACT(year from user_votes.created_at)"
 			when "month"
@@ -38,18 +38,17 @@ class Api::V1::Analyzes::VoteStatisticsController < ApplicationController
 			end
 
 			today_participant_count = UserVote.where("DATE(user_votes.created_at) = ? AND vote_id = ?", DateTime.now.to_date, vote_id).count
-			user_vote =  User.joins("LEFT JOIN user_votes ON users.id = user_votes.user_id AND user_votes.vote_id = #{vote_id} 
-				
-				")
+			#user_vote =  User.joins("LEFT JOIN user_votes ON users.id = user_votes.user_id AND user_votes.vote_id = #{vote_id} ")
 			user_vote_cat =  UserVote.joins(:user).where("user_votes.vote_id  = ?", vote_id)
 			top_education = user_vote_cat.order("count_all DESC").group(:degree).count.try(:first).try(:first) || ""
 			top_profesion = user_vote_cat.order("count_all DESC").group(:job).count.try(:first).try(:first) || ""
+			top_region = user_vote_cat.order("count_all DESC").group(:city).count.try(:first).try(:first) || ""
 
 			modus_choice = vote.vote_options.as_json
 			modus_choice = modus_choice.max_by{|e| e[:total_voter]}
 			modus_choice = modus_choice[:options]
-			
-			filtered = user_vote.group(grouped_query, grouped_query_y).count
+
+			filtered = user_vote_cat.group(grouped_query, grouped_query_y).count
 			max_category = user_vote_cat.group(grouped_query_y).order("count_all DESC").count.try(:first) || ""
 			max_category = ["", ""] if max_category.nil?
 			min_category = user_vote_cat.group(grouped_query_y).order("count_all ASC").count.try(:first) || ""
@@ -68,10 +67,10 @@ class Api::V1::Analyzes::VoteStatisticsController < ApplicationController
 			}
 			hash_result_final = {}
 			if hash_extra.count > 0
-				hash_result.each { |e| 
+				hash_result.each { |e|
 					tmp_hash = {}
-					hash_extra.each { |e1| 
-						if !e.second.key?(e1.first) 
+					hash_extra.each { |e1|
+						if !e.second.key?(e1.first)
 							tmp_hash = tmp_hash.merge(e.second.merge({"#{e1.first}"=> e1.second}))
 						else
 							tmp_hash = tmp_hash.merge(e.second)
@@ -82,7 +81,7 @@ class Api::V1::Analyzes::VoteStatisticsController < ApplicationController
 			else
 				hash_result_final = hash_result
 			end
-			
+
 
 			#filtered = UserVote.joins(:vote_option).where("vote_options.vote_id = ?", vote_id).group(grouped_query, :vote_option).count if y_filter== "options"
 			render json:{
@@ -94,6 +93,7 @@ class Api::V1::Analyzes::VoteStatisticsController < ApplicationController
 						participant_count: participant_count,
 						today_participant_count: today_participant_count,
 						top_profesion: top_profesion,
+						top_region: top_region,
 						top_education: top_education,
 						modus_choice: modus_choice,
 						max_value: max_category,
