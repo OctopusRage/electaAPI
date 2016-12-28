567140aa3c5ea5654fe9fc1e4333d89cbce2fa4a
octopus <ahyan1995a@gmail.com>
2016-12-28 02:54:05 -0500

    improve message

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,18 +4,18 @@ class Message < ActiveRecord::Base
 	validates :subject, presence: true
 	validates :message, presence: true
 	def as_json(options={})
-		to_name = User.find_by(id: to.to_i).try(:name)
-		to_email = User.find_by(id: to.to_i).try(:email)
-		from_name = User.find_by(id: from.to_i).try(:name)
-		from_email = User.find_by(id: from.to_i).try(:email)
+		to_name = User.find(to.to_i).try(:name)
+		to_email = User.find(to.to_i).try(:email)
+		from_name = User.find(from.to_i).try(:name)
+		from_email = User.find(from.to_i).try(:email)
 		{
 			id: id,
 			to: to,
 			to_name: to_name,
 			to_email: to_email,
 			from: from,
-			from_name: to_name,
-			from_email: to_email,
+			from_name: from_name,
+			from_email: from_email,
 			subject: subject,
 			message: message,
 			created_at: created_at
